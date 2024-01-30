34cb6a706c79e68b6d1fed4ce92ff2a13882c550
Sengshin Lee <sengshinlee@gmail.com>
2024-01-31 00:11:04 +0800

    Update

--- wireguard.sh
+++ wireguard.sh
@@ -26,18 +26,18 @@ cd mirrors
 
 WG_WINDOWS_PATH="https://download.wireguard.com/windows-client/"
 WG_WINDOWS_AMD64=$(curl -s ${WG_WINDOWS_PATH} | cut -d '"' -f 4)
-aria2c -c -t 1000 -s 1000 -o wireguard-windows-latest-amd64.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_AMD64}
+aria2c -c -t 10 -s 1000 -o wireguard-windows-latest-amd64.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_AMD64}
 WG_WINDOWS_ARM64=$(curl -s ${WG_WINDOWS_PATH} | cut -d '"' -f 6)
-aria2c -c -t 1000 -s 1000 -o wireguard-windows-latest-arm64.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_ARM64}
+aria2c -c -t 10 -s 1000 -o wireguard-windows-latest-arm64.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_ARM64}
 WG_WINDOWS_X86=$(curl -s ${WG_WINDOWS_PATH} | cut -d '"' -f 6)
-aria2c -c -t 1000 -s 1000 -o wireguard-windows-latest-x86.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_X86}
+aria2c -c -t 10 -s 1000 -o wireguard-windows-latest-x86.msi ${WG_WINDOWS_PATH}${WG_WINDOWS_X86}
 
 WG_ANDROID_PATH="https://download.wireguard.com/android-client/"
 WG_ANDROID_VER=$(curl -s ${WG_ANDROID_PATH} | \
                 awk -F '.apk' '{print $2}' | \
                 cut -d '-' -f 2)
 WG_ANDROID_URL=${WG_ANDROID_PATH}"com.wireguard.android-"${WG_ANDROID_VER}".apk"
-aria2c -c -t 1000 -s 1000 -o wireguard-android-latest.apk ${WG_ANDROID_URL} 
+aria2c -c -t 10 -s 1000 -o wireguard-android-latest.apk ${WG_ANDROID_URL} 
 
 cat >wireguard-ubuntu-latest.sh <<WG_UBUNTU_EO'F'
 #!/bin/bash
